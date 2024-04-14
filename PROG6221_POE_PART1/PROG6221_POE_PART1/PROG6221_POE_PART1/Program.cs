029e2f7b9c6dab7dea76205a4bd52b5f7a40752d
Keaton Jones <st10336767@vcconnect.edu.za>
2024-04-14 12:24:43 +0200

    Try catch
    I have added a try catch to check the users have entered the correct data.

--- PROG6221_POE_PART1/PROG6221_POE_PART1/PROG6221_POE_PART1/Program.cs
+++ PROG6221_POE_PART1/PROG6221_POE_PART1/PROG6221_POE_PART1/Program.cs
@@ -28,7 +28,7 @@ namespace PROG6221_POE_PART1
                 switch (choice)//Switch statement to handle the user's choice
                 {
                     case "1"://If the user chooses to add a recipe
-                         EnterRecipeDetails();//Calling the EnterRecipeDetails method
+                        EnterRecipeDetails();//Calling the EnterRecipeDetails method
                         break;
                     case "2"://If the user chooses to display a recipe
                         recipe.DisplayRecipe();//Calling the DisplayRecipe method
@@ -54,17 +54,41 @@ namespace PROG6221_POE_PART1
         }
 
         public static void EnterRecipeDetails()//Method to enter the recipe details
-        {
+        {      
             Console.WriteLine("Enter the name of the recipe: ");//Prompt the user to enter the name of the recipe
             String name = Console.ReadLine();//Read the name of the recipe
-
+            
             int numIngredients = 0;//Variable to store the number of ingredients
             int numSteps = 0;//Variable to store the number of steps
             Console.WriteLine("Enter the number of ingredients: ");//Prompt the user to enter the number of ingredients
             String numOfIngredients = Console.ReadLine();//Read the number of ingredients
             Console.WriteLine("Enter the number of steps: ");//Prompt the user to enter the number of steps
             String numOfSteps = Console.ReadLine();//Read the number of steps
-            
+            try
+            {
+                if (numOfIngredients == "" || numOfSteps == "")
+                {
+                    throw new Exception("Number of ingredients cannot be 0");//Throw an exception if the user enters an empty string
+                }
+
+                if (!numOfIngredients.All(char.IsDigit) || !numOfSteps.All(char.IsDigit))
+                {
+                    throw new Exception("Please enter a valid number");//Throw an exception if the user enters a non-numeric value
+                    
+                }
+                else
+                {
+                    numIngredients = Convert.ToInt32(numOfIngredients);//Convert the number of ingredients to an integer
+                    numSteps = Convert.ToInt32(numOfSteps);//Convert the number of steps to an integer
+                }
+
+                
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);//Display the error message
+                EnterRecipeDetails();//Calling the EnterRecipeDetails method
+            }
+           
         }
     }
 }
