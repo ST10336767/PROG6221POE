be9d3ddefbf077fb3d32d34fe3e1fe38817bb3c5
Keaton Jones <st10336767@vcconnect.edu.za>
2024-04-14 12:19:15 +0200

    Switch Case
    I have added a switch case to handle the user's choice and run the required method chosen by the user. I have also created a Recipe object.

--- PROG6221_POE_PART1/PROG6221_POE_PART1/PROG6221_POE_PART1/Program.cs
+++ PROG6221_POE_PART1/PROG6221_POE_PART1/PROG6221_POE_PART1/Program.cs
@@ -9,9 +9,9 @@ namespace PROG6221_POE_PART1
 {
     class Program
     {
-
         static void Main(string[] args)//Main method
         {
+            Recipe recipe = new Recipe();//Initializing the recipe object to null
             bool exit = false;//Boolean variable to control the loop
             while (!exit)//While loop to keep the program running
             {
@@ -24,7 +24,34 @@ namespace PROG6221_POE_PART1
                 Console.WriteLine("6. Exit");//Displaying a menu option
                 String choice = Console.ReadLine();//Reading the user's choice
 
+                switch (choice)//Switch statement to handle the user's choice
+                {
+                    case "1"://If the user chooses to add a recipe
+                        recipe = EnterRecipeDetails();//Calling the EnterRecipeDetails method
+                        break;
+                    case "2"://If the user chooses to display a recipe
+                        recipe.DisplayRecipe();//Calling the DisplayRecipe method
+                        break;
+                    case "3"://If the user chooses to scale a recipe
+                        recipe.ScaleRecipe();//Calling the ScaleRecipe method
+                        break;
+                    case "4"://If the user chooses to reset the scale
+                        recipe.ResetScale();//Calling the ResetScale method
+                        break;
+                    case "5"://If the user chooses to delete a recipe
+                        recipe.DeleteRecipe();//Calling the DeleteRecipe method
+                        break;
+                    case "6"://If the user chooses to exit the program
+                        exit = true;//Setting the exit variable to true
+                        break;
+                    default://If the user enters an invalid choice
+                        Console.WriteLine("Invalid choice. Please try again.");//Displaying an error message
+                        break;
+                }   
+
             }
         }
+
+        
     }
 }
