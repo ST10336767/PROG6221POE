9ddd64601da31d9e6d1d7bd840534fdb007e7f94
Keaton Jones <st10336767@vcconnect.edu.za>
2024-04-14 12:16:51 +0200

    Welcome message and Menu
    I have added a welcome message and menu option to the main class (Program.cs). Along with a Boolean variable to terminate the program when the user selects 6.

--- PROG6221_POE_PART1/PROG6221_POE_PART1/PROG6221_POE_PART1/Program.cs
+++ PROG6221_POE_PART1/PROG6221_POE_PART1/PROG6221_POE_PART1/Program.cs
@@ -12,6 +12,19 @@ namespace PROG6221_POE_PART1
 
         static void Main(string[] args)//Main method
         {
+            bool exit = false;//Boolean variable to control the loop
+            while (!exit)//While loop to keep the program running
+            {
+                Console.WriteLine("Welcome to the Recipe Manager");//Displaying a welcome message
+                Console.WriteLine("1. Add a recipe");//Displaying a menu option
+                Console.WriteLine("2. Display recipe");//Displaying a menu option
+                Console.WriteLine("3. Scale recipe (0.5/2/3)");//Displaying a menu option
+                Console.WriteLine("4. Reset scale");//Displaying a menu option
+                Console.WriteLine("5. Delete recipe");//Displaying a menu option
+                Console.WriteLine("6. Exit");//Displaying a menu option
+                String choice = Console.ReadLine();//Reading the user's choice
+
+            }
         }
     }
 }
